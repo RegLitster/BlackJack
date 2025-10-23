916c0d9ef085236d54160834b65be301e6c7b812
Reggie <reglitster00@gmail.com>
2025-10-23 15:51:54 -0400

    EOD Commit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,34 +1,107 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         Deck deck = new Deck();
-        Hand player1Hand = new Hand();
-        Hand player2Hand = new Hand();
 
         homeScreen();
         deck.shuffle();
 
-        System.out.println("Please Enter Player1 Name");
-        String player1 = scanner.nextLine();
-        System.out.println("Please Enter Player2 Name");
-        String player2 = scanner.nextLine();
+        ArrayList<String> playerNames = new ArrayList<>();
+        ArrayList<Hand> playerHands = new ArrayList<>();
+
+
+        System.out.println("Please Enter Number of Players: ");
+        int playerCount = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < playerCount; i++) {
+            System.out.println("Please Enter Player Name: ");
+            String playerName = scanner.nextLine();
+            playerNames.add(playerName);
+            playerHands.add(new Hand());
+        }
 
         for (int i = 0; i < 2; i++) {
-
-            player1Hand.deal(deck.deal());
-            player2Hand.deal(deck.deal());
+            for (Hand hand : playerHands) {
+                hand.deal(deck.deal());
+            }
         }
-        int handValue = player1Hand.getValue();
-        System.out.println(player1+ " Hand is worth: " + handValue);
+
+        ArrayList<Integer> handValue = new ArrayList<>();
+        for (int i = 0; i < playerCount; i++) {
+            int value = playerHands.get(i).getValue();
+            handValue.add(value);
+            System.out.println(playerNames.get(i) + " Hand Is Worth: " + value+"\n");
+        }
+        hit(playerNames, playerHands, deck);
+        scoring(playerNames, playerHands);
 
     }
+
     public static void homeScreen() {
         System.out.println("==== Welcome To BlackJack ====");
     }
+
+    public static void scoring(ArrayList<String> playerNames, ArrayList<Hand> playerHands) {
+        int highestScore = 0;
+        ArrayList<String> winningPlayerNames = new ArrayList<>();
+
+        for (int i = 0; i < playerNames.size(); i++) {
+            int value = playerHands.get(i).getValue();
+
+            if (value > 21) { //this skips a player with a score over 21
+                continue;
+            }
+
+            if (value > highestScore) {
+                highestScore = value;
+                winningPlayerNames.clear();
+                winningPlayerNames.add(playerNames.get(i));
+            } else if (value == highestScore) {
+                winningPlayerNames.add(playerNames.get(i));
+            }
+        }
+        if (winningPlayerNames.size() == 1) {
+            System.out.println(winningPlayerNames.get(0) + " Wins!");
+        } else {
+            System.out.println("It's a Push");
+        }
+
+
+    }
+
+    public static void hit(ArrayList<String> playerNames, ArrayList<Hand> playerHands, Deck deck) {
+        for (int i = 0; i < playerNames.size(); i++) {
+            System.out.println(playerNames.get(i) + " Your Hand Is Worth "+playerHands.get(i).getValue()+ " Would you Like to Hit? Y/N");
+            String playerChoice = scanner.nextLine().toUpperCase().trim();
+
+            while (true) {
+                if (playerChoice.equals("N")) {
+                    System.out.println(playerNames.get(i) + " Hand Is Still Worth: " + playerHands.get(i).getValue());
+                    break;
+
+                } else if (playerChoice.equals("Y")) {
+                    playerHands.get(i).deal(deck.deal());
+                    int value = playerHands.get(i).getValue();
+                    System.out.println(playerNames.get(i) + " Hand Is Now Worth: " + value);
+                    if (value > 21) {
+                        System.out.println("Bust!");
+                        break;
+                    }
+                }
+            }
+
+
+        }
+
+
+    }
+
 }
 
 
